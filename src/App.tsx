36762d99ad3eda68d7ae962ae0c1e4c29a14b77f
Shaurya <smilingshaurya@gmail.com>
2023-05-31 21:59:27 +0530

    Init command

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import NewPost from "./components/NewPost";
+import PostsList from "./components/PostsList";
+
+import "./App.css";
+
+interface TODO {
+  id: string;
+  work: string;
+}
 
 function App() {
+  const [list, setList] = useState<TODO[]>([]);
+  const addToList = (value: string) => {
+    setList((prevList) => [
+      ...prevList,
+      { id: Math.random().toString(), work: value },
+    ]);
+  };
+  const deleteTaskHandler = (id: string) => {
+    setList((prevList) => {
+      return prevList.filter((task) => task.id !== id);
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewPost addTodo={addToList} />
+      <PostsList allTasks={list} deletetask={deleteTaskHandler} />
     </div>
   );
 }
